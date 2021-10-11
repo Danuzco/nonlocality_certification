193f68565823a02be5ba11ec0f94f15a57471be0
Danuzco <danuzco@gmail.com>
2021-10-10 22:50:56 -0300

    documentation

--- codes_nlc.py
+++ codes_nlc.py
@@ -9,7 +9,7 @@ from datetime import datetime
 cwd = os.getcwd()
 
 
-def optimal_value(filename, m, num_of_outcomes, num_of_trials, marginals_A, 
+def optimal_value(filename, m, num_of_outcomes, num_of_trials, F, marginals_A, 
                   marginals_B, disp = False, save = False):
     
     """
@@ -37,12 +37,20 @@ def optimal_value(filename, m, num_of_outcomes, num_of_trials, marginals_A,
         filename + datetime.today().strftime('_%Y-%m-%d') )
     dir3 = ( cwd + '\\npy_data\\' + 'sby_' + 
         filename + datetime.today().strftime('_%Y-%m-%d') )
+    dir4 = ( cwd + '\\npy_data\\' + 'p_' + 
+        filename + datetime.today().strftime('_%Y-%m-%d') )
+    dir5 = ( cwd + '\\npy_data\\' + 'pax_' + 
+        filename + datetime.today().strftime('_%Y-%m-%d') )
+    dir6 = ( cwd + '\\npy_data\\' + 'pby_' + 
+        filename + datetime.today().strftime('_%Y-%m-%d') )
+    dir7 = ( cwd + '\\npy_data\\' + 'Q_C_Dq_Gap' + 
+        filename + datetime.today().strftime('_%Y-%m-%d') )
     
     print('n: number of iterations')
     print(60*'_')
     print(f'    n       R         Q         C         \u0394Q         Gap')
     print(60*'_')
-    p, c, _ = load_data(filename, m, num_of_outcomes, 
+    p, c, _ = load_data(filename, F, m, num_of_outcomes, 
                         marginals_A, marginals_B)
     s0, bounds = initial_guess(m, num_of_outcomes, 
                                marginals_A, marginals_B) # initial guess for the optimizer
@@ -55,7 +63,7 @@ def optimal_value(filename, m, num_of_outcomes, num_of_trials, marginals_A,
         res = minimize( R , s0 ,  args=(p, c, m, num_of_outcomes, marginals_A, marginals_B), 
                         method='SLSQP', tol = 1e-20, 
                         options={ 'ftol':1e-20, 'maxiter':5000, 'disp': False}, 
-                        bounds=bounds, constraints=[nlc]) 
+                        bounds=bounds, constraints=[nlc])
         s0 = (res.x + s0)/2  # average between the target guess 's0' and the current solution
                              # R = -res.fun. Thus, 'res.fun < target' => 'R > -target'
         if  res.fun < target:
@@ -65,18 +73,26 @@ def optimal_value(filename, m, num_of_outcomes, num_of_trials, marginals_A,
             nlc = NonlinearConstraint( lambda x: -target  
                                       + R( x, p, c, m, num_of_outcomes, marginals_A, marginals_B), 
                                       -np.inf, -target ) # update the constraints
+            
             Q, C, Dq, gap = results( solution.x, p, c, m, 
                                     num_of_outcomes, marginals_A, marginals_B)
             s, sax, sby = vector_to_tensor(solution.x, p, m, num_of_outcomes, marginals_A, marginals_B)
             # save solutions at the 'npy_data' folder
+            pabxy, pax, pby = p
             if save:
                 np.save(dir1, s)
                 np.save(dir2, sax)
                 np.save(dir3, sby)
+                np.save(dir4, pabxy)
+                np.save(dir5, pax)
+                np.save(dir6, pby)
+                np.save(dir7, np.array([Q, C, Dq, gap]) )
+
             # display results
             if disp:
                 print( "%5d    %4.5f   %4.5f   %4.5f    %4.5f    %4.5f" 
                       %(n, -solution.fun, Q, C, Dq, gap) )    
+               
         n += 1
     
     if SolutionFound:
@@ -218,7 +234,7 @@ def R( s, p, c, m, num_of_outcomes, marginals_A, marginals_B):
     Returns:
     - R
     """
-    k = (num_of_outcomes**2)*(m**2)
+    k = (num_of_outcomes)**2*(m)**2
     s = vector_to_tensor(s, p, m, num_of_outcomes, marginals_A, marginals_B)
     Q = quantum_max_violation(s, p)
     C = lhv_value(s, m, num_of_outcomes )
@@ -306,7 +322,7 @@ def results( s, p, c, m, num_of_outcomes, marginals_A, marginals_B):
 
 
 
-def load_data(name_of_file, m, num_of_outcomes, marginals_A, marginals_B):
+def load_data(name_of_file, F, m, num_of_outcomes, marginals_A, marginals_B):
     """
     Load the experimental countings saved in "countings_C[C].txt". The file is located in the "experimental_data" folder.
     Each file consists of two columns: 
@@ -370,11 +386,20 @@ def load_data(name_of_file, m, num_of_outcomes, marginals_A, marginals_B):
     for l in labels_counts:
         a,b,x,y = l
         a,b,x,y = list( map( int,l ) )
-        if measured_marginals:
-            pax[int(a), int(x)] = cax[int(a), int(x)]/( np.sum( cax[:,int(x)] ) )
-            pby[int(b), int(y)] = cby[int(b), int(y)]/( np.sum( cby[:,int(y)] ) )
         p[a,b,x,y] = c[a,b,x,y]/( np.sum( c[:,:,x,y] ) )
     
+    p1 = p
+    num_of_trials = 10
+    output = optimal_settings_KL_divergence(F, p, num_of_outcomes, m, num_of_trials)
+    p = get_probs_from_settings(output.x,m)
+    
+    if measured_marginals:
+        for l in labels_counts:
+            a,b,x,y = l
+            a,b,x,y = list( map( int,l ) )
+            pax[int(a), int(x)] = cax[int(a), int(x)]/( np.sum( cax[:,int(x)] ) )
+            pby[int(b), int(y)] = cby[int(b), int(y)]/( np.sum( cby[:,int(y)] ) )
+
     if len(marginals_A) > 0:
         for x in marginals_A:
             for a in range(2):
@@ -383,7 +408,132 @@ def load_data(name_of_file, m, num_of_outcomes, marginals_A, marginals_B):
         for y in marginals_B:
             for b in range(2):
                 pby[b,y] = np.sum(p[:,b ,0 ,y])
-    
+                 
     return (p, pax, pby) , ( c, cax, cby ), measured_marginals
+
+
+
+
+# Kullback-Leible divergence
+
+def U(x):
+    
+    beta, gamma, alpha= x[0], x[1], x[2]
+    return np.array( [ [ np.exp( -1j*gamma )*np.cos( beta/2 ) , - np.exp( 1j*alpha )*np.sin( beta/2 ) ], 
+                    [ np.exp( -1j*alpha )*np.sin( beta/2 ), np.exp( 1j*gamma )*np.cos(beta/2) ] ] )   
+
+
+def W(theta):
+    
+    ket0 = np.array([1,0])
+    ket1 = np.array([0,1])
+    psi = np.cos(theta)*np.kron(ket0,ket0) + np.sin(theta)*np.kron(ket1,ket1)
+    
+    return np.outer( psi, np.conjugate( psi ) )
+
+
+def get_probs_from_settings(x, m):
+    
+    o = 2
+    theta, x = x[0],  x[1:]
+    rho = W(theta)
+    Us = np.array( list( map(U, x.reshape( int(x.shape[0]/3) ,3) ) ) )
+    Ua, Ub = Us[:int(Us.shape[0]/2)], Us[int(Us.shape[0]/2):]
+    
+    pa, pb, p = np.zeros((o,m)), np.zeros((o,m)), np.zeros((o,o,m,m))
+    for i in itertools.product(range(m), repeat = 2):
+        for l in itertools.product(range(o), repeat = 2):
+            x,y = i
+            a,b = l
+            Pa = np.outer( Ua[x][:,a], np.conjugate( Ua[x][:,a] ) )
+            Pb = np.outer( Ub[y][:,b], np.conjugate( Ub[y][:,b] ) )
+            p[a,b,x,y] = np.real( np.trace( rho @ np.kron(Pa, Pb) ) )
+    
+    return  p
+
+
+def initial_data(m, num_of_parties):
+    
+    lb , ub = 0, np.pi
+    x0 = np.array( [(ub - lb) * np.random.random_sample() 
+                    + lb for i in range(3*num_of_parties*m)] )
+    
+    lb , ub = 0, np.pi
+    theta = (ub - lb) * np.random.random_sample() + lb
+    x0 = np.insert(x0 , 0, theta )
+    
+    ### Bounds
+    lb , ub = 0, np.pi
+    x_bounds = np.array( [(0, np.pi/4)] 
+                        + [(lb, ub) for i in range(3*num_of_parties*m)] )
+    
+    return x0, x_bounds
+
+
+def kullback_leibe_divergence(x, F, f, num_of_outcomes, m):
+    
+    """
+    f: relative frequencies (original probabilities)
+    """
+    p = get_probs_from_settings(x, m)
+    
+    D = 0
+    for i in itertools.product(range(num_of_outcomes), repeat = m):
+        for j in itertools.product(range(m), repeat = m):
+            a,b = i
+            x,y = j
+            D += F(x,y)*f[a,b,x,y]*(np.log2(f[a,b,x,y])-np.log2(p[a,b,x,y]))
+            
+    return D
+
+
+
+def optimal_settings_KL_divergence(F, probs, num_of_outcomes, 
+                                               m, num_of_trials):
+    
+    """
+    This routine minimize the Kullback-Leibe divergence.
+    It optimizes over the parameters in the settings.
+    """
+    
+    # one can also use COBYLA in this optimization
+    # just comment the next line and uncomment the 
+    # line corresponding to COBYLA.
+    
+    OPTIMIZER = 'SLSQP' 
+#     OPTIMIZER = 'COBYLA'
+
+    # initial guess and bounds for the optimizer
+    x0, bounds = initial_data(num_of_outcomes, m)
+    threshold = 0
+    cons = ({'type': 'ineq', 'fun': lambda x: threshold 
+             - kullback_leibe_divergence(x, F, probs, num_of_outcomes, m)} )
+
+    res = minimize( kullback_leibe_divergence , x0 ,  
+                    args=(F,probs, num_of_outcomes, m), 
+                    method=OPTIMIZER, tol = 1e-20, 
+                    options={ 'maxiter':250, 'disp': False},
+                   constraints=cons
+                  )
+
+    previous = res.fun
+    result = res
+    for i in range(num_of_trials):
+        
+        x0 = (res.x + x0)/2
+        res = minimize( kullback_leibe_divergence , x0 ,  
+                    args=(F, probs, num_of_outcomes, m), 
+                    method=OPTIMIZER, tol = 1e-20, 
+                    options={ 'maxiter':250, 'disp': False},
+                    constraints=cons
+                  )
+        
+        # save the best result
+        if res.fun < previous:
+            previous = res.fun
+            result = res
+    
+    return result
+
 
 
